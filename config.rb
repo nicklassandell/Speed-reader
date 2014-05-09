b9c789e23d4b13596cbc5c23ae5794aa624fe4ed
Nicklas Sandell <nike.sandell@gmail.com>
2014-05-09 19:28:58 +0200

    Ditched foundation completely. It's been a good run my friend, but the time has come.

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-# Require any additional compass plugins here.
-add_import_path "bower_components/foundation/scss"
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
